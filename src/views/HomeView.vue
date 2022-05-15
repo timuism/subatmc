03a21e21989697ae9e16dd538c14bfb5966dec17
Tim Long <timuism@me.com>
2022-05-15 16:09:36 -0400

    mobile responsive styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,13 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-    <main class="w-auto py-32 h-screen flex divide-x-4 divide-primary/25">
-        <aside class="w-1/2 px-12 flex items-center justify-center">
+    <main class="flex flex-col w-auto h-screen px-4 py-32 divide-y-4 md:px-0 md:divide-y-0 md:divide-x-4 md:flex-row divide-primary/25 min-w-[320px]">
+        <aside class="flex items-center justify-center w-auto px-12 pb-24 mx-auto md:w-1/2 md:mx-0 md:pb-0">
             <img src="@/assets/SubatmcLogo.svg" alt="" class="animate-pulse" />
         </aside>
 
-        <section class="w-1/2 lowercase space-y-24 flex flex-col items-center justify-center">
+        <section class="flex flex-col items-center justify-center w-auto px-12 pt-24 mx-auto space-y-24 lowercase md:w-1/2 md:mx-0 md:py-0">
             <div class="space-y-4">
-                <h2 class="italic text-3xl opacity-25">Listen Now</h2>
-                <a href="https://song.link/us/i/1574353490" target="_new" class="inline-block text-7xl underline hover:scale-110 duration-500">
+                <h2 class="text-3xl italic opacity-25">Listen Now</h2>
+                <a href="https://song.link/us/i/1574353490" target="_new" class="inline-block underline duration-500 text-7xl hover:scale-110">
                     Earth House
                 </a>
             </div>
