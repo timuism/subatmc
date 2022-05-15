d6ff9b8d2833cff9cd423abf984b3a5a750a7215
Tim Long <timuism@me.com>
2022-05-15 16:12:54 -0400

    add padding, small mobile font for link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,10 @@
             <img src="@/assets/SubatmcLogo.svg" alt="" class="animate-pulse" />
         </aside>
 
-        <section class="flex flex-col items-center justify-center w-auto px-12 pt-24 mx-auto space-y-24 lowercase md:w-1/2 md:mx-0 md:py-0">
+        <section class="flex flex-col items-center justify-center w-auto px-12 pt-24 pb-24 mx-auto space-y-24 lowercase md:pb-0 md:w-1/2 md:mx-0 md:py-0">
             <div class="space-y-4">
                 <h2 class="text-3xl italic opacity-25">Listen Now</h2>
-                <a href="https://song.link/us/i/1574353490" target="_new" class="inline-block underline duration-500 text-7xl hover:scale-110">
+                <a href="https://song.link/us/i/1574353490" target="_new" class="inline-block text-5xl underline duration-500 md:text-7xl hover:scale-110">
                     Earth House
                 </a>
             </div>
