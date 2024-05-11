1e61661255ee758e10f35724b9bad109f4ea8c85
Tim Long <timuism@me.com>
2024-05-11 01:30:25 -0400

    add 40 hours

--- components/content/MusicCard.vue
+++ components/content/MusicCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 defineProps<{
     title: string
+    link: string
     releaseDate: string
     image: string
     imageAlt: string
@@ -8,18 +9,18 @@ defineProps<{
 </script>
 
 <template>
-    <NuxtLink to="/"
+    <a :href="link" target="_blank"
         class="cursor-pointer group relative grid grid-cols-1 grid-rows-1 rounded-lg overflow-hidden w-full h-[200px] md:h-full">
         <div
-            class="col-start-1 row-start-1 z-10 w-full text-white flex justify-start items-end duration-500 bg-gradient-to-t from-black/25 to-transparent group-hover:from-black/95">
-            <div class="opacity-0 duration-500 group-hover:opacity-100 group-focus:opacity-100 p-6">
+            class="z-10 flex items-end justify-start w-full col-start-1 row-start-1 text-white duration-500 bg-gradient-to-t from-black/25 to-transparent group-hover:from-black/95">
+            <div class="p-6 duration-500 opacity-0 group-hover:opacity-100 group-focus:opacity-100">
                 <h2 class="font-sans text-sm uppercase">{{ title }}</h2>
             </div>
         </div>
 
-        <div class="col-start-1 row-start-1 w-full pointer-events-none">
+        <div class="w-full col-start-1 row-start-1 pointer-events-none">
             <img :src="image" :alt="imageAlt" class="object-cover w-full h-full"
                 :class="[title === '??? (Coming Soon)' ? 'blur-lg' : '']" />
         </div>
-    </NuxtLink>
+      </a>
 </template>
