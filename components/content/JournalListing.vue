e6a19e98e912030682be6582b13553b09a6cfdb4
Tim Long <timuism@me.com>
2024-10-01 23:16:48 -0400

    more fixes

--- components/content/JournalListing.vue
+++ components/content/JournalListing.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: '/_posts', sort: [{ date: -1 }] }
+</script>
 <template>
   <div>
     <ul class="space-y-12">
-      <ContentList path="/_posts">
+      <ContentList :query="query">
         <template #default="{list}">
         <li v-for="article in list" :key="article._path">
           <NuxtLink :to="article._path" class="flex flex-col justify-start px-1 duration-500 bg-transparent rounded-sm md:space-x-4 md:items-center md:justify-center md:flex-row group hover:bg-gray-800/50">
