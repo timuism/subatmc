299c14609c7142a015326f4834b43077131b925d
Tim Long <timuism@me.com>
2024-10-01 23:08:15 -0400

    mobile fix for listing page

--- components/content/JournalListing.vue
+++ components/content/JournalListing.vue
@@ -4,12 +4,12 @@
       <ContentList path="/_posts">
         <template #default="{list}">
         <li v-for="article in list" :key="article._path">
-          <NuxtLink :to="article._path" class="flex items-center px-1 space-x-4 duration-500 bg-transparent rounded-sm group hover:bg-gray-800/50">
+          <NuxtLink :to="article._path" class="flex flex-col justify-start px-1 duration-500 bg-transparent rounded-sm md:space-x-4 md:items-center md:justify-center md:flex-row group hover:bg-gray-800/50">
             <h2 class="text-lg text-gray-400 duration-100 group-hover:text-gray-100">
               {{ article.title }}
             </h2>
 
-            <hr class="mt-1 border-t border-gray-700 border-dotted grow" />
+            <hr class="hidden mt-1 border-t border-gray-700 border-dotted md:block grow" />
 
             <p class="text-base tracking-wider text-gray-600 uppercase duration-100 group-hover:text-gray-100">
               {{ article.date }}
